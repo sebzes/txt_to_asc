b9c4186535795824d85126c844d88b1d5afb9a12
sebzes <132289921+sebzes@users.noreply.github.com>
2024-12-25 18:44:51 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-### BEFEJEZNI A CLIPBOARD FUNKCIOT
+### A vágólapra másolt PGP titkosított üzenetet írja bele egy .asc fájlba
+## Használata meggyorsítja a PGP titkosított üzeneteknek a fájlbaírását a gyorsabb üzenetváltás érdekében 
--- main.py
+++ main.py
@@ -3,43 +3,47 @@ import os
 import time
 import pyperclip
 
-print("Írja be a PGP titkosított üzenetet a PGPMESSAGE.txt fájlba!\nMajd nyomjon ENTERT")
+os.system('cls')
+
+print("Másolja ki a PGP titkosított üzenetet a vágólapra\nMajd nyomjon ENTERT")
 input()
 
 def convert():
-    clipboard = pyperclip.paste()
     date = datetime.datetime.now()
-    try:
-        with open("PGPMESSAGE.txt", "r") as file:
-            content = file.read()
-        if content == "":
-            return "NODATA"
-        else:
-            dirName = f"{date.year}_{date.month}_{date.day}"
-            os.mkdir(f"{dirName}")
-            fileName = f"{date.year}{date.month}{date.day}_{date.hour}_{date.minute}_{date.second}.asc"
-            sv = open(f"{fileName}", "w")
-            sv.write(content)
-            sv.close()
-            print(f"{fileName} fájl létrehozva!")
-            os.system(f"move {fileName} {dirName}")
+    content = pyperclip.paste()
 
-    except FileExistsError:
-        with open("PGPMESSAGE.txt", "r") as file:
-            content = file.read()
-        if content == "":
-            return "NODATA"
+    if content != "":
+        if content.startswith("-----BEGIN PGP MESSAGE-----"):
+            try:
+                dirName = f"{date.year}_{date.month}_{date.day}"
+                os.mkdir(f"{dirName}")
+                fileName = f"{date.year}{date.month}{date.day}_{date.hour}_{date.minute}_{date.second}.asc"
+                sv = open(f"{fileName}", "w")
+                sv.write(content)
+                sv.close()
+                print(f"{fileName} fájl létrehozva!")
+                os.system(f"move {fileName} {dirName}")
+
+            except FileExistsError:
+                content = pyperclip.paste()
+                fileName = f"{date.year}{date.month}{date.day}_{date.hour}_{date.minute}_{date.second}.asc"
+                sv = open(f"{fileName}", "w")
+                sv.write(content)
+                sv.close()
+                print(f"{fileName} fájl létrehozva!")
+                os.system(f"move {fileName} {dirName}")
         else:
-            fileName = f"{date.year}{date.month}{date.day}_{date.hour}_{date.minute}_{date.second}.asc"
-            sv = open(f"{fileName}", "w")
-            sv.write(content)
-            sv.close()
-            print(f"{fileName} fájl létrehozva!")
-            os.system(f"move {fileName} {dirName}")
+            return "NOTPGPMESSAGE"
+    else:
+        return "NODATA"
 
 def start():
     result = convert()
+    
     if result == "NODATA":
-        print("Nincs egy karakter sem a PGPMESSAGE.txt fájlban!")
+        print("A vágólap üres")
+    
+    elif result == "NOTPGPMESSAGE":
+        print("A vágólapon nem PGP titkosított üzenet szerepel")
 
 start()
